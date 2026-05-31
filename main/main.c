10b08301a3ef2ceb3ebd8c227daf0a674a31a3b7
mikus <michalkremowka@o2.pl>
2026-05-31 19:52:02 +0200

    added typedefs & new structs & started using pointer for cleaner code in score & overall cleaner code

--- main/main.c
+++ main/main.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <stdbool.h>
+#include <windows.h>
 
-struct Scores {
+typedef struct {
+    bool gameRunning;
     int userScore;
     int AIScore;
-};
+} gameValues;
 
-struct mapCoordinates {
+typedef struct {
     int size_X;
     int size_Y;
-};
+} mapCoordinates;
 
-struct ballCoordinates {
+typedef struct {
     int ball_X;
     int ball_Y;
-};
+    bool moveUpwards;
+} ballCoordinates;
 
 
 void initMenu();
 void startGame();
 void updateMap(int size_X, int size_Y, int ball_X, int ball_Y);
-void gameOver();
+void Score(gameValues* values, ballCoordinates* ballCords);
+
 
 int main() {
 
     int userChoice = 0;
-    bool gameRunning = false;
 
     initMenu();
 
@@ -37,7 +38,6 @@ int main() {
     switch (userChoice) {
         case 1:
             startGame();
-            gameRunning = true;
             break;
         case 2:
             printf("Highscores");
@@ -62,28 +62,37 @@ void initMenu() {
 
 void startGame() {
     system("cls");
-    struct ballCoordinates ballCords;
-    struct mapCoordinates map;
+
+    ballCoordinates ballCords;
+    mapCoordinates map;
+    gameValues values;
+
+    values.gameRunning = true;
+    values.userScore = 0;
+    values.AIScore = 0;
+
     map.size_X = 32;
     map.size_Y = 10;
 
     //default
-    bool moveUpwards = false;
+    ballCords.moveUpwards = false;
     ballCords.ball_X = 15; // 15 because 0 and 32 is wall
     ballCords.ball_Y = 5;
 
-    while (true) {
-        sleep(1);
+    while (values.gameRunning) {
+        Sleep(750);
+        if (ballCords.ball_X == 0) {
+            Score(&values, &ballCords);
+        }
         if (ballCords.ball_X == map.size_X + 1) {
-            gameOver();
-            break;
+            Score(&values, &ballCords);
         }
 
         if (ballCords.ball_Y >= map.size_Y - 1) {
-            moveUpwards = true;
+            ballCords.moveUpwards = true;
         }
 
-        if(!moveUpwards) {
+        if(!ballCords.moveUpwards) {
             ballCords.ball_X+=2;
             ballCords.ball_Y++;
         } else {
@@ -98,6 +107,8 @@ void startGame() {
 void updateMap(int size_X, int size_Y, int ball_X, int ball_Y) 
 {
     system("cls");
+    printf("Ball X: %d \n", ball_X);
+    printf("Ball Y: %d \n", ball_Y);
     for (int y = 0; y <= size_Y; y++) { // Y
         for (int x = 0; x <= size_X; x++) { // X
 
@@ -132,12 +143,33 @@ void updateMap(int size_X, int size_Y, int ball_X, int ball_Y)
 
         printf("\n");
     }
-
-    printf("Ball X: %d \n", ball_X);
-    printf("Ball Y: %d \n", ball_Y);
 }
 
-void gameOver() {
-    system("cls");
-    printf("Game over!");
+void Score(gameValues* values, ballCoordinates* ballCords) {
+    if (ballCords->ball_X == 0) 
+    {
+        values->gameRunning = false;
+        values->AIScore++;
+
+        system("cls");
+        printf("Ai scored!: %d", values->AIScore);
+        Sleep(1000);
+
+        ballCords->ball_X = 15;
+        ballCords->ball_Y = 5;
+        ballCords->moveUpwards = false;
+        values->gameRunning = true;
+    } else {
+        values->gameRunning = false;
+        values->userScore++;
+
+        system("cls");
+        printf("User scored!: %d", values->userScore);
+        Sleep(1000);
+
+        ballCords->ball_X = 15;
+        ballCords->ball_Y = 5;
+        ballCords->moveUpwards = false;
+        values->gameRunning = true;
+    }
 }
